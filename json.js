f2cdb0fdbc65384e47293583094e36fe01c15463
praveendr <81307771+praveendr@users.noreply.github.com>
2021-06-02 20:08:25 +0530

    Add files via upload

---
+++ json.js
@@ -0,0 +1,12 @@
+// JSON Document
+data = {"quizcontent": [
+        {"question": "What is the name of the biggest planet in our solar system", "image": "1.jpg", "correct": 1, "a1":"jupiter", "a2":"mars","a3":"Sun","a4":"Earth"},
+    {"question": "In which year of First World War Germany declared war on Russia and France", "image": "2.jpg", "correct": 3, "a1":"1915", "a2":"1918","a3":"1914","a4":"1920"},
+	{"question": "India has largest deposits of ____ in the world", "image": "3.jpg", "correct": 2, "a1":"copper", "a2":"mica","a3":"none","a4":"gold"},
+	{"question": "India's tallest stone statue of the Jain sage Gomateswara is at", "image": "4.jpg", "correct": 3, "a1":"Mysore, Karnakata", "a2":"New Delhi","a3":"Sravanabelagola, Karnataka","a4":"Mandu, Madhya Pradesh"},
+	{"question": "In a normal human body, the total number of red blood cells is", "image": "5.jpg", "correct": 1, "a1":"30 trillion", "a2":"20 trillion","a3":"10 trillion","a4":"1 trillion"},
+	{"question": "In which season do we need more fat?", "image": "3.jpg", "correct": 1, "a1":"Winter", "a2":"summer","a3":"rainy","a4":"spring"},
+	{"question": "How many teeth does a normal adult dog have?", "image": "3.jpg", "correct": 2, "a1":"44", "a2":"42","a3":"77","a4":"55"},
+	{"question": "How many red blood cells does the bone marrow produce every second?", "image": "3.jpg", "correct": 2, "a1":"1 million", "a2":"2 million","a3":"4 million","a4":"8 million"},
+	],
+};
